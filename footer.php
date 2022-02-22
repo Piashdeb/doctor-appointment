d64d4920ec0410248ad5962a9362bb2e5dfb14dd
unknown <Piashdeb>
2022-02-23 00:24:50 +0600

    seccond commit

--- footer.php
+++ footer.php
@@ -1,26 +1,33 @@
    <!-- footer section -->
    <footer>
    	<div class="col-md-12 fsection">
-   		<div class="col-md-4" id="contact">
+   		
+		   <div class="col-md-4" id="contact">
    			<p>Category</p>
    			<hr>
    			<ul>
    				<li><a href="signin.php">Search for Doctors</a></li>
    				<li><a href="signin.php">Contact with Doctors</a></li>
+				<li> <a href="admin/adminlogin.php">Admin Login</a></li>
 
    			</ul>
-   		</div>
+   			</div>
    		<div class="col-md-4">
    			<p>Contact</p>
    			<hr>
    			<p>Admin<br>
    				admin123@gmail.com <br>
-   				Cell: +880 1700000000</p>
-   			<p>
-   				Copyright &copy;<script>
+   				Cell: +880 1700000000
+			</p>
+			<br>
+			<br>
+
+			<p>
+   					Copyright &copy;<script>
    					document.write(new Date().getFullYear());
-   				</script> All rights reserved </a>
-   			</p>
+   					</script> All rights reserved </a>
+   				</p>
+   			
    		</div>
    		<div class="col-md-4 share_img">
    			<p>Link With</p>
@@ -28,9 +35,17 @@
    			<a href=""><img src="img/fb-free.png" alt="facebok"></a>
    			<a href=""><img src="img/gogle-plud-free.png" alt="google-plus"></a>
    			<a href=""><img src="img/twitter.png" alt="twitter"></a>
+			   <br>
+			   <br>
+			   <br>
+			   
 
    		</div>
+			
+		
+		
    	</div>
+	   	
 
 
    </footer>
